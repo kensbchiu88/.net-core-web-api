2a84cc5c1a2fbd2a2543bea3f6101627d4b903e6
FIT1037 <ken.sp.chiu@fit-foxconn.com>
2023-11-03 10:19:29 +0800

    Feature: 開發綁定載具接口 BIND_FIXTURE_BY_SN_FIXTURE

--- PolarBearEapApi/ApplicationCore/Services/FitMesService.cs
+++ PolarBearEapApi/ApplicationCore/Services/FitMesService.cs
@@ -12,51 +12,57 @@ namespace PolarBearEapApi.ApplicationCore.Services
             _equipmentService = equipmentService;
         }
 
-        async Task<string> IMesService.ADD_BOM_DATA(string pLineName, string pSectionCode, string pStationCode, string pSN, string pComponentLot)
+        public async Task<string> ADD_BOM_DATA(string pLineName, string pSectionCode, string pStationCode, string pSN, string pComponentLot)
         {
             var result = await Task.Run( () => _equipmentService.ADD_BOM_DATA(pLineName, pSectionCode, pStationCode, pSN, pComponentLot));
             return result;
         }
 
-        async Task<string> IMesService.CHECK_OP_PASSWORD(string pUserID, string pPassword)
+        public async Task<string> CHECK_OP_PASSWORD(string pUserID, string pPassword)
         {
             var result = await Task.Run(() => _equipmentService.CHECK_OP_PASSWORD(pUserID, pPassword));
             return result;
         }
 
-        async Task<string> IMesService.CHECK_SECTION_PERMISSION(string pOpeartorID, string pSectionCode, string pStationCode)
+        public async Task<string> CHECK_SECTION_PERMISSION(string pOpeartorID, string pSectionCode, string pStationCode)
         {
             var result = await Task.Run(() => _equipmentService.CHECK_SECTION_PERMISSION(pOpeartorID, pSectionCode, pStationCode));
             return result;
         }
 
-        async Task<string> IMesService.GET_SN_BY_SN_FIXTURE(string pCarrierNo)
+        public async Task<string> GET_SN_BY_SN_FIXTURE(string pCarrierNo)
         {
             var result = await Task.Run(() => _equipmentService.GET_SN_BY_SN_FIXTURE(pCarrierNo));
             return result;
         }
 
-        async Task<string> IMesService.UNIT_PROCESS_CHECK(string pSN, string pSectionCode, string pStationCode)
+        public async Task<string> UNIT_PROCESS_CHECK(string pSN, string pSectionCode, string pStationCode)
         {
             var result = await Task.Run(() => _equipmentService.UNIT_PROCESS_CHECK(pSN, pSectionCode, pStationCode));
             return result;
         }
 
-        async Task<string> IMesService.UNIT_PROCESS_COMMIT(string pLineName, string pSectionCode, string pStationCode, string pSN)
+        public async Task<string> UNIT_PROCESS_COMMIT(string pLineName, string pSectionCode, string pStationCode, string pSN)
         {
             var result = await Task.Run(() => _equipmentService.UNIT_PROCESS_COMMIT(pLineName, pSectionCode, pStationCode, pSN));
             return result;
         }
 
-        async Task<string> IMesService.UNIT_PROCESS_COMMIT(string pLineName, string pSectionCode, string pStationCode, string pSN, string pResult, string pList_of_failing_tests, string pFailure_Message)
+        public async Task<string> UNIT_PROCESS_COMMIT(string pLineName, string pSectionCode, string pStationCode, string pSN, string pResult, string pList_of_failing_tests, string pFailure_Message)
         {
             var result = await Task.Run(() => _equipmentService.UNIT_PROCESS_COMMIT(pLineName, pSectionCode, pStationCode, pSN, pResult, pList_of_failing_tests, pFailure_Message));
             return result;
         }
 
-        async Task<string> IMesService.GET_SN_BY_SMTSN(string pSMTSN)
+        public async Task<string> GET_SN_BY_SMTSN(string pSMTSN)
         {
             var result = await Task.Run(() => _equipmentService.GET_SN_BY_SMTSN(pSMTSN));
+            return result;
+        }
+
+        public async Task<string> BIND_FIXTURE_BY_SN_FIXTURE(string pLineName, string pSectionCode, string pStationCode, string pCarrierNo, string pBindCarrierNo)
+        {
+            var result = await Task.Run(() => _equipmentService.BIND_FIXTURE_BY_SN_FIXTURE(pLineName, pSectionCode, pStationCode, pCarrierNo, pBindCarrierNo));
             return result;
         }
     }
